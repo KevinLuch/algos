dd5acaa89fc84e0785906a6783a6df63948dc1d1
Kevin <kevinluch@gmail.com>
2023-01-05 05:33:58 -0600

    edited files

---
+++ day_3.js
@@ -0,0 +1,11 @@
+// Write some code that loops through and prints all the values in an array
+
+function print(arr){
+    for(i = 0; i < arr.length; i++ ){
+        console.log(arr[i]);
+    }
+}
+
+
+console.log(print([1,2,3]))
+console.log(print([1,2,3,4,5]))
