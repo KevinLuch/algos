5979be8a108cd874309945b4ad96ffe2722b2d4c
Kevin <kevinluch@gmail.com>
2023-01-06 06:29:44 -0600

    edited files

---
+++ day_3.py
@@ -0,0 +1,20 @@
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+# print_full_name has the following parameters:
+
+# string first: the first name
+# string last: the last name
+# Prints string: 'Hello  ! You just delved into python' where  and  are replaced with first and last
+
+def print_full_name(first, last):
+
+    print("Hello " + first + " " + last + "! You just delved into python.")
+
+
+print(print_full_name("Ross", "Taylor"))
+print(print_full_name("Kevin", "Luch"))
