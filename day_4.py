4fa9b95af03493d5590ba2da6a9bdc5ba2296904
Kevin <kevinluch@gmail.com>
2023-01-16 06:53:42 -0600

    edited files

---
+++ day_4.py
@@ -0,0 +1,16 @@
+
+
+n = int(input())
+arr = map(int, input().split())
+
+scores = list(arr)
+
+scores.sort()
+first, second = -100, 100
+
+for score in scores:
+    if first < score:
+        second = first
+        first = score
+
+print(second)
