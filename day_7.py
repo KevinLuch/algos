2db77a611c330afbb0fa869381e3a873d6152ab5
Kevin <kevinluch@gmail.com>
2023-01-19 20:41:17 -0600

    edied files

---
+++ day_7.py
@@ -0,0 +1,14 @@
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
